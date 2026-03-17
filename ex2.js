c9a11c4c00dd09407cd5fccd1805e90d2a62bdda
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-03-17 12:15:17 -0300

    ADD - ex1

---
+++ ex2.js
@@ -0,0 +1,10 @@
+/*A NASA precisa de sua ajuda para realizar um lançamento de foguete.
+Crie um algoritmo que faça uma contagem regressiva para o lançamento, iniciando em 10 e indo até 0.
+Ao final da contagem, exiba a mensagem:
+"Lançamento realizado!"*/
+
+function nasa(){
+
+    for
+
+}
