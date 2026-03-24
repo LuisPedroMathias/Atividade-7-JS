d07192301a9e1623ddbfd97aaa94a9dcbafce841
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-03-24 12:30:01 -0300

    ADD - ex6

---
+++ Scripts/ex6.js
@@ -0,0 +1,11 @@
+/*➔ Crie um algoritmo que peça ao usuário uma letra do alfabeto.
+O programa deverá verificar se a letra digitada é:
+➔ Vogal
+➔ Consoante
+E mostrar o resultado no console.*/
+
+function ex6() {
+
+    let letra = (prompt("Digite uma letra: "));
+
+}
