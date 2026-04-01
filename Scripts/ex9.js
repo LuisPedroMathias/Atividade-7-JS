20f714312e34819a55a6afe01aebc3f9623f48ba
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-04-01 12:01:55 -0300

    ADD - ex9

---
+++ Scripts/ex9.js
@@ -0,0 +1,12 @@
+function ex9(){
+
+    let nu1 = prompt ('Digite um número:');
+    let nu2 = prompt ('Digite um número:');
+
+    if(nu1>nu2){
+        console.log(nu1,nu2);
+    }else{
+        console.log(nu2,nu1);
+    }
+
+}
