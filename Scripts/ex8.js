2e7f8d6e8af96964b9e5f25ea48463533c3a193f
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-04-01 11:58:03 -0300

    ADD - ex8

---
+++ Scripts/ex8.js
@@ -0,0 +1,10 @@
+function ex8(){
+
+    let nu1 = prompt ('Digite um número:');
+    let nu2 = prompt ('Digite um número:');
+
+    console.log( nu1 - nu2);
+    console.log((2*nu1)+(3*nu2));
+    console.log(nu1*nu2);
+
+}
